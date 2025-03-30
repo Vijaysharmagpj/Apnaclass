974492e4023bb55df5c7cb4db3b6da2cd3ff9603
Vijaysharmagpj <vijaysharmagpj65@gmail.com>
2025-03-30 23:23:47 +0530

    first commit

---
+++ models/RatingAndReview.js
@@ -0,0 +1,22 @@
+const { model } = require("mongoose");
+
+const mongoose=required("mongoose");
+
+const ratingAndReviewSchema=new mongoose.Schema({
+    User:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"User"
+    },
+    rating:{
+        type:Number,
+        required:true,
+    },
+    review:{
+        type:String,
+        required:true,
+        trim:true,
+    }
+})
+
+module.exports=mongoose.model("RatingAndReview",ratingAndReviewSchema)
