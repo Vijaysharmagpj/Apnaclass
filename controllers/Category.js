8a91ac0e611d1ff2903d8b82725e2812add05bc2
Vijaysharmagpj <vijaysharmagpj65@gmail.com>
2025-07-10 14:03:06 +0530

    category page deatils controller

--- controllers/Category.js
+++ controllers/Category.js
@@ -54,3 +54,54 @@ exports.showAllCategory = async (req, res) => {
     });
   }
 };
+
+//category page details
+exports.categoryPageDetails=async(req,res)=>{
+  try {
+    //get category id
+    const {categoryId}=req.body;
+    //get course for specified category id
+    const selectedcategory=await Category.findById({categoryId}).populate("course").exec();
+    // validation
+    if(!selectedcategory){
+      return res.status(404).json({
+        success:false,
+        message:"Data not found"
+      })
+    }
+    // get course for diffrent Category 
+    const diffrentCategory=await Category.find({
+      _id:{$ne:categoryId},
+    }).populate("course").exec();
+    // get top selling courses
+          const allCategories = await Category.find()
+        .populate({
+          path: "course",
+          match: { status: "Published" },
+          populate: "ratingAndReviews",
+        })
+        .exec()
+      const allCourses = allCategories.flatMap((category) => category.course)
+      const mostSellingCourses = await Course.find({ status: 'Published' })
+      .sort({ "studentsEnrolled.length": -1 }).populate("ratingAndReviews") 
+      .exec();
+      console.log(allCourses);
+
+    //return res
+    res.status(200).json({
+      success:true,
+			selectedCourses: selectedCourses,
+			differentCourses: differentCourses,
+			mostSellingCourses,
+            name: selectedCourses.name,
+            description: selectedCourses.description,
+            success:true
+		})
+  } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+      success:false,
+      message:error.message,
+        })
+  }
+}
