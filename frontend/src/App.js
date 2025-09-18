56f920bc6b3cc945bdfb08e983bcf489501f7c77
Vijaysharmagpj <vijaysharmagpj65@gmail.com>
2025-09-18 16:18:54 +0530

    home page section 1 and sector 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import {Route,Routes} from "react-router-dom"
+import Home from "../src/pages/Home"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="w-screen min-h-screen bg-[#000814] flex flex-col font-inter">
+    <Routes>
+      <Route path='/' element={<Home/>} />
+    </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/components/core/HomePage/CodeBlocks.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import CTAButton from "./Button"
+import HighlightText from './HighlightText'
+import { FaArrowRight } from "react-icons/fa"
+import { TypeAnimation } from 'react-type-animation'
+
+const CodeBlocks = ({ position, heading, subheading, CTAButton1, CTAButton2, CodeBlocks, backgroundGradient, codecolor }) => {
+    return (
+        <div className={`flex ${position} my-20 justify-between flex-col gap-10`}>
+            {/*Section 1*/}
+            <div className='w-[100%] lg:w-[50%] flex flex-col gap-8'>
+                {heading}
+                <div className='text-richblack-300 text-base font-bold w-[85%] -mt-3 '>
+                    {subheading}
+                </div>
+
+                <div className='flex gap-7 mt-7'>
+                    <CTAButton active={CTAButton1.active} linkto={CTAButton1.linkto}>
+                        <div className='flex gap-2 items-center'>
+                            {CTAButton1.btnText}
+                            <FaArrowRight />
+                        </div>
+                    </CTAButton>
+
+                    <CTAButton active={CTAButton2.active} linkto={CTAButton2.linkto}>
+                        {CTAButton2.btnText}
+                    </CTAButton>
+                </div>
+            </div>
+            {/* section 2 */}
+            <div className=' h-fit code-border flex flex-row py-3 text-[10px] sm:text-sm leading-[18px] sm:leading-6 relative w-[100%] lg:w-[470px]'>
+                <div className='absolute gradient-custom w-[373px] h-[257px] rounded-full blur-2xl opacity-20 -left-2 -top-2'></div>
+                <div className='text-center flex select-none flex-col w-[10%] text-richblack-400 font-inter font-bold'>
+                    <p>1</p>
+                    <p>2</p>
+                    <p>3</p>
+                    <p>4</p>
+                    <p>5</p>
+                    <p>6</p>
+                    <p>7</p>
+                    <p>8</p>
+                    <p>9</p>
+                    <p>10</p>
+                    <p>11</p>
+                </div>
+                <div className={`w-[90%] flex flex-col gap-2 font-bold 
+        font-mono ${codecolor} pr-1`}>
+                    <TypeAnimation
+                        sequence={[CodeBlocks, 2000, ""]}
+                        repeat={Infinity}
+                        cursor={true}
+                        style={
+                            {
+                                whiteSpace: "pre-line",
+                                display: "block",
+                            }
+                        }
+                        omitDeletionAnimation={true}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CodeBlocks
